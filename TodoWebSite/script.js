53e1cdb3b5db07b5eff753909ea07e9af1d6a083
Mark Heath <mark.heath@gmail.com>
2018-07-21 13:28:52 +0100

    calling through to azure functions api

--- TodoWebSite/script.js
+++ TodoWebSite/script.js
@@ -3,25 +3,43 @@ var app = new Vue({
     el: '#app',
     data: {
         todos: [
-            { id: '1', TaskDescription: 'Performance Tuning', IsComplete: true },
-            { id: '2', TaskDescription: 'Learn Vue', IsComplete: false },
-            { id: '3', TaskDescription: 'Eat lunch', IsComplete: false },
         ],
-        newTask: ""
+        newTask: "",
+        error: undefined
     },
     methods: {
         createTodo: function() {
-            this.todos.push({ id: `${this.todos.length + 1}`, 
-                TaskDescription: this.newTask, IsComplete: false });
-            this.newTask = '';
+            fetch(`${baseAddress}/api/todo`, { method: "POST",
+                body: JSON.stringify({ taskDescription: this.newTask} )})
+                .then(response => response.json())
+                .then(json => {
+                    this.todos.push(json);
+                    this.newTask = '';
+                })
+                .catch(reason => this.error = `Failed to create item: ${reason}`);
         },
         deleteTodo: function(todo) {
-            var index = this.todos.indexOf(todo);
-            if (index > -1) {
-              this.todos.splice(index, 1);
-            }
+            var todos = this.todos;
+            fetch(`${baseAddress}/api/todo/${todo.id}`, { method: "DELETE"})
+                .then(function() {
+                    var index = todos.indexOf(todo);
+                    if (index > -1) {
+                        todos.splice(index, 1);
+                    }
+                })
+                .catch(reason => this.error = `Failed to delete item: ${reason}`);
         },
-        
-
-    }
+        updateTodo: function(todo) {
+            const body = JSON.stringify({ isCompleted: todo.isCompleted });
+            fetch(`${baseAddress}/api/todo/${todo.id}`, 
+                { method: "PUT", body: body})
+                .catch(reason => this.error = `Failed to update item: ${reason}`);
+        },     
+    },
+    mounted: function () {
+        fetch(`${baseAddress}/api/todo`)
+            .then(response => response.json())
+            .then(json => this.todos = json)
+            .catch(reason => this.error = `Failed to fetch todos: ${reason}`);
+    },
 });
