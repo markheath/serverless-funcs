3b484f9021fea6b0df513dcacf65446d4ddac3ad
Mark Heath <mark.heath@gmail.com>
2018-07-21 10:26:11 +0100

    adding the todo models

---
+++ ServerlessFuncs/Models.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ServerlessFuncs
+{
+    public class Todo
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class TodoCreateModel
+    {
+        public string TaskDescription { get; set; }
+    }
+}
